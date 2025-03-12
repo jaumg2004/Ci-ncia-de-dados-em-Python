d7d62d7a0f1f1b22a690edf230dc34bb21de7699
João Gabriel Ferreira Ribeiro <151750450+jaumg2004@users.noreply.github.com>
2025-03-11 22:03:59 -0300

    Update Lista 4.py

--- Lista 4.py
+++ Lista 4.py
@@ -17,7 +17,8 @@ print(f'Média do valor das missões em milhões: {data_cost.sum(dtype=float)/da
 print('Exercício 3')
 location_column = data[:, 2]
 location_USA = np.char.find(location_column, 'USA')
-print(f'Quantidade de missões realizadas nos EUA: {location_USA[location_USA != -1].size}\n')
+tam = np.sum(location_USA != -1)
+print(f'Quantidade de missões realizadas nos EUA: {tam}\n')
 
 print('Exercício 4')
 details_column = data[1:, 4]
@@ -25,15 +26,15 @@ company_column = data[1:, 1]
 space_x = np.where(company_column == 'SpaceX')
 space_x_cost = cost_mission_column[space_x]
 space_x_details = details_column[space_x]
-details_cost = ", ".join(space_x_details[np.where(space_x_cost == max(space_x_cost))])
-print(f'As missões mais caras da SpaceX foram {details_cost}\n')
+details_cost = "; ".join(space_x_details[np.where(space_x_cost == max(space_x_cost))])
+print(f'As missões mais caras da SpaceX foram: {details_cost}\n')
 
 print('Exercício 5')
 company = np.unique(company_column)
 company_count = [np.sum(company_column == comp) for comp in company]
 print('Empresas que já realizaram missões no espaço e quantidade de missões\n')
 for comp, count in zip(company, company_count):
-    print(f'Nome: {comp} Missões: {count}')
+    print(f'Nome: {comp} ; Missões: {count}')
 
 
 
